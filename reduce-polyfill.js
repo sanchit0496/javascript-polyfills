07711c87f4ac645d3c1047318b1f083a2394ea2e
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-18 17:00:53 +0530

    have improved reduce polyfill

--- reduce-polyfill.js
+++ reduce-polyfill.js
@@ -1,19 +1,20 @@
 console.log('hello')
 
 
-Array.prototype.customReduce = function(callback){
+Array.prototype.customReduce = function(callback, initialValue){
     console.log(callback, this)
     let sum = 0
     for(let i = 0; i < this.length; i++){
-        sum += this[i]
+        sum += callback(initialValue, this[i])
     }
     return sum
 }
 
-const addElements = (currentElement, index, inputArray) => {
-    return currentElement + inputArray[index+ 1]
+const addElements = (accumulator, currentElement) => {
+    return accumulator + currentElement
 }
+
 let arr = [1,2,3,4,5,6,7,8,9,10]
-let totalSum = arr.customReduce(addElements)
+let totalSum = arr.customReduce(addElements, 0)
 
 console.log(totalSum)
