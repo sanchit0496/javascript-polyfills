6736afe2dd1ff14e00b2891e77e37d4f8c4eec81
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-18 18:25:14 +0530

    added basic code for includes polyfill here

--- includes-polyfill.js
+++ includes-polyfill.js
@@ -1,1 +1,15 @@
-console.log('hello')
+Array.prototype.customIncludes = function (inputElement) {
+  console.log(inputElement, this);
+  for(let i = 0; i < this.length; i++){
+    if(this[i] === inputElement){
+        return true
+    }
+  }
+  return false
+};
+
+
+let arr = [1, 2, 3, 4, 5];
+
+let retVal = arr.customIncludes(1);
+console.log(retVal);
